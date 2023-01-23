c8e4eef8ac2ba5dc2d96b3ad4c7dd3895f2489f6
Lilian de Leon <deleonarleth17@gmail.com>
2023-01-22 23:44:50 -0500

    change

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
-
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
 
-setInterval (function() {
-    if (direction === 'west') {
-    x = x - 1
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
     }
-    if (direction === 'north') {
-    y = y + 1
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
     }
-    if (direction === 'east') {
-    x = x + 1
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
     }
-    if (direction === 'south') {
-    y = y - 1 
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
     }
-character.style.left = x + 'px'
-character.style.bottom = y + 'px'
-}, i)
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
 
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
-//move(character).to(100, 250)
-document.addEventListener('keydown', function(e) {
-    
-})
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
